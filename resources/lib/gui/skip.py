67cf9b86cb5e1944b511804bc79c403783a80e70
Michal Paulovic <coolmip@gmail.com>
2022-09-12 20:23:47 +0200

    2.2.09.1
    fix skip button on TV

--- resources/lib/gui/skip.py
+++ resources/lib/gui/skip.py
@@ -13,10 +13,18 @@ class Skip(xbmcgui.WindowXMLDialog):
     def is_button_visible(self):
         return self.is_visible
 
+    def onInit(self):  # type: () -> None
+        self.setFocus(self.getControl(3001))
+
     def onClick(self, control_id):
         debug('SKIP onclick: ' + str(control_id))
-        if control_id == 1:
+        if control_id == 3001:
             self.callback()
+
+    def onAction(self, action):  # type: (Action) -> None
+        # if action ==
+        debug("action: {}".format(action.getId()))
+        pass
 
     def set_visibility(self):
         self.is_visible = not self.is_visible
